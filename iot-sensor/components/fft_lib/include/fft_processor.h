dabf40d6f335231c8312e3cbe789576387edb111
lellosant <leonardosantucci99@gmail.com>
2026-05-12 16:52:35 +0200

    update gitignore update fft_processor.c

--- iot-sensor/components/fft_lib/include/fft_processor.h
+++ iot-sensor/components/fft_lib/include/fft_processor.h
@@ -8,22 +8,22 @@
 extern "C" {
 #endif
 
-#define FFT_PROCESSOR_SAMPLES 1024 
-
 /**
  * @brief Inizializza il modulo processore FFT e alloca le risorse necessarie.
  * 
+ * @param[in] max_samples Numero massimo di campioni previsti (deve essere potenza di 2).
  * @return ESP_OK se l'inizializzazione ha successo, altrimenti un codice di errore.
  */
-esp_err_t fft_processor_init(void);
+esp_err_t fft_processor_init(int max_samples);
 
 /**
  * @brief Elabora un segnale nel dominio del tempo ed estrae le magnitudo delle frequenze.
  * 
- * @param[in]  input_signal     Array contenente il segnale di input (dimensione: FFT_PROCESSOR_SAMPLES).
- * @param[out] output_magnitude Array dove verranno salvate le magnitudo (dimensione: FFT_PROCESSOR_SAMPLES / 2).
+ * @param[in]  input_signal     Array contenente il segnale di input (dimensione: samples).
+ * @param[out] output_magnitude Array dove verranno salvate le magnitudo (dimensione: samples / 2).
+ * @param[in]  samples          Numero di campioni (es. 1024, potenza di 2).
  */
-void fft_processor_compute_magnitude(float *input_signal, float *output_magnitude);
+void fft_processor_compute_magnitude(float *input_signal, float *output_magnitude, int samples);
 
 #ifdef __cplusplus
 }
